06694152faecf0f5ca071aea26e43e3119a00abc
marianabs3 <62958830+marianabs3@users.noreply.github.com>
2020-04-03 13:27:25 -0300

    Modificação jogo

--- jogo_de_craps.py
+++ jogo_de_craps.py
@@ -1,39 +1,96 @@
 import random
-fichas = int(input('Vamos começar o jogo de Craps. Com quantas fichas você gostaria de começar? '))
+print('Nesse jogo quando aparecer a opção [s/n], responda "s" para sim e "n" para não')
+fichas = int(input('Vamos começar o jogo de Craps. Com quantas fichas você gostaria de começar?\n'))
 while fichas <= 0:
-    fichas = int(input('Vamos começar o jogo de Craps. Com quantas fichas você gostaria de começar? '))
+    fichas = int(input('Vamos começar o jogo de Craps. Com quantas fichas você gostaria de começar?\n'))
 continua_jogando = 's'
-while continua_jogando=='s':
+while continua_jogando =='s':
     print('Fase de Come Out')
-    pass_line_bet = srt(input('Você gostaria de fazer uma aposta Pass Line Bet? [s/n] '))
+    pass_line_bet = srt(input('Você gostaria de fazer uma aposta Pass Line Bet? [s/n]\n'))
     while pass_line_bet not in 'sn':
-        pass_line_bet = srt(input('Você gostaria de fazer uma aposta Pass Line Bet? [s/n] '))
+        pass_line_bet = srt(input('Você gostaria de fazer uma aposta Pass Line Bet? [s/n]\n'))
         if pass_line_bet == 's':
-            fichas_pass_line_bet = int(input('Quantas fichas gostaria de apostar em Pass Line Bet? '))
+            fichas_pass_line_bet = int(input('Quantas fichas gostaria de apostar em Pass Line Bet?\n'))
             fichas = fichas - fichas_pass_line_bet
         while fichas < 0:
             fichas = fichas + fichas_pass_line_bet
-            fichas_pass_line_bet = int(input('Você não tem fichas suficientes. Tente novamente. '))
+            fichas_pass_line_bet = int(input('Você não tem fichas suficientes. Tente novamente.\n'))
             fichas = fichas - fichas_pass_line_bet
             print('Suas fichas: {}'.format(fichas))
-    field = srt(input('Você gostaria de fazer uma aposta em Field?[s/n] '))
+    field = srt(input('Você gostaria de fazer uma aposta em Field? [s/n]\n'))
     while field not in 'sn':
-        field = srt(input('Você gostaria de fazer uma aposta Pass Line Bet? [s/n] '))
-        if field == 's':
-            fichas_field = int(input('Quantas fichas gostaria de apostar em Pass Line Bet? '))
-            fichas = fichas - fichas_field
-        while fichas < 0:
-            fichas = fichas + fichas_field
-            fichas_field = int(input('Você não tem fichas suficientes. Tente novamente. '))
-            fichas = fichas - fichas_field
-            print('Suas fichas: {}'.format(fichas))
-    twelve = str(input('Você gostaria de fazer uma aposta em Twelve?[s/n] '))
+        field = srt(input('Você gostaria de fazer uma aposta Pass Line Bet? [s/n]\n'))
+    if field == 's':
+        fichas_field = int(input('Quantas fichas gostaria de apostar em Pass Line Bet?\n'))
+        fichas = fichas - fichas_field
+    while fichas < 0:
+        fichas = fichas + fichas_field
+        fichas_field = int(input('Você não tem fichas suficientes. Tente novamente.\n'))
+        fichas = fichas - fichas_field
+        print('Suas fichas: {}'.format(fichas))
+    any_craps = str(input('Você gostaria de fazer uma aposta Any Craps? [s/n]\n'))
+    while any_craps not in 'sn':
+        any_craps = str(input('Você gostaria de fazer uma aposta Any Craps? [s/n]\n'))
+    if any_craps == 's':
+        fichas_any_craps = int(input('Quantas fichas você quer apostar em Any Craps?\n'))
+        fichas = fichas - fichas_any_craps
+    while fichas < 0:
+        fichas = fichas + fichas_any_craps
+        fichas_any_craps = int(input('Você não tem fichas suficiente, tente novamente.\n'))
+        fichas = fichas - fichas_any_craps
+        print('Suas fichas: {}'.format(fichas))
+    twelve = str(input('Você gostaria de fazer uma aposta em Twelve? [s/n]\n '))
     while twelve not in 'ns':
-        twelve = str(input('Você gostaria de fazer uma aposta em Twelve?[s/n] '))
+        twelve = str(input('Você gostaria de fazer uma aposta em Twelve? [s/n]\n '))
     if twelve == 's':
-        fichas_twelve = int(input('Quantas fichas você quer apostar em Twelve? '))
+        fichas_twelve = int(input('Quantas fichas você quer apostar em Twelve?\n'))
         fichas = fichas - fichas_twelve
-        
+    while fichas < 0:
+        fichas = fichas + fichas_twelve
+        fichas_twelve = int(input('Você não tem fichas suficiente, tente novamente.\n'))
+        fichas = fichas - fichas_twelve
+        print('Suas fichas: {}'.format(fichas))
+
+    d1 = randint(1, 6)
+    d2 = randint(1, 6)
+    sd = d1 + d2
+    print('Rolagem de dados:\nPrimeiro dado - {}\nSegundo dado - {}\nSoma - {}'.format(d1, d2, sd))
+
+    if field == 's':
+        if sd == 3 or sd == 4 or sd == 9 or sd == 10 or sd == 11:
+            fichas = fichas + 2 * fichas_field
+            print('Você venceu no Field, recuperou suas {} fichas e ganhou a mais a mesma quantidade.'.format(fichas_field))
+            print('Suas fichas: {}'.format(fichas))
+        elif sd == 2:
+            fichas = fichas + 3 * fichas_field
+            print('Você venceu no Field, recuperou suas {} fichas e ganhou a mais o dobro dessa quantidade.'.format(fichas_field))
+            print('Suas fichas: {}'.format(fichas))
+        elif sd == 12:
+            fichas = fichas + 4 * fichas_field
+            print('Você venceu no Field, recuperou suas {} fichas e ganhou a mais o triplo dessa quantidade.'.format(fichas_field))
+            print('Suas fichas: {}'.format(fichas))
+        else:
+            print('Você perdeu no Field.')
+     if any_craps == 's':
+        if sd == 3 or sd == 2 or sd == 12:
+            fichas = fichas + 8 * fichas_any_craps
+            print('Você venceu no Any Craps, recuperou suas {} fichas e ganhou a mais sete vezes a mesma quantidade.'.format(fichas_any_craps))
+            print('Suas fichas: {}'.format(fichas))
+        else:
+            print('Você perdeu no Any Craps.')
+
+    if twelve == 's':
+        if sd == 12:
+            fichas = fichas + 31 * fichas_twelve
+            print('Você venceu no Twelve, recuperou suas {} fichas e ganhou a mais trinta vezes a mesma quantidade.'.format(fichas_twelve))
+            print('Suas fichas: {}'.format(fichas))
+        else:
+            print('Você perdeu no Twelve.')
+    if pass_lineb_bet == 's':
+        if sd == 7 or sd == 11:
+            fichas = fichas + 2 * fichas_pass_line_bet
+            print('Você venceu no Pass Line Bet, recuperou suas {} fichas e ganhou a mais a mesma quantidade.'.format(fichas_pass_line_bet))
+            print('Suas fichas: {}'.format(fichas))
     
 
 
